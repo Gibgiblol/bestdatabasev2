520ab9ff9c6b1a1bacf4299d54812998af293dbd
Charles <charlesclei21@gmail.com>
2018-03-30 22:59:19 -0600

    test api

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
@@ -7,21 +6,20 @@ var ObjectID = mongodb.ObjectID;
 var CONTACTS_COLLECTION = "contacts";
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
   // Save database object from the callback for reuse.
-  db = database;
+  db = client.db();
   console.log("Database connection ready");
 
   // Initialize the app.
@@ -39,38 +37,52 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
-/*  "/contacts"
+/*  "/api/contacts"
  *    GET: finds all contacts
  *    POST: creates a new contact
  */
 
-app.get("/contacts", function(req, res) {
+app.get("/api/contacts", function(req, res) {
 });
 
-app.post("/contacts", function(req, res) {
+app.post("/api/contacts", function(req, res) {
 });
 
-/*  "/contacts/:id"
+/*  "/api/contacts/:id"
  *    GET: find contact by id
  *    PUT: update contact by id
  *    DELETE: deletes contact by id
  */
 
-app.get("/contacts/:id", function(req, res) {
+app.get("/api/contacts/:id", function(req, res) {
 });
 
-app.put("/contacts/:id", function(req, res) {
+app.put("/api/contacts/:id", function(req, res) {
 });
 
-app.delete("/contacts/:id", function(req, res) {
+app.delete("/api/contacts/:id", function(req, res) {
 });
 
-app.post("/contacts", function(req, res) {
+/*  "/api/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+
+app.get("/api/contacts", function(req, res) {
+  db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/contacts", function(req, res) {
   var newContact = req.body;
-  newContact.createDate = new Date();
 
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
   }
 
   db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
