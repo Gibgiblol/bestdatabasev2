96650dde1e5ee0a764a285a807345230a24f0011
Charles <charlesclei21@gmail.com>
2018-03-31 02:49:45 -0600

    I think its working now

--- server.js
+++ server.js
@@ -1,83 +1,46 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongodb.ObjectID;
+var express = require('express');
+var parser = require('body-parser');
 
-var CONTACTS_COLLECTION = "contacts";
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://charles:charles123@ds231199.mlab.com:31199/bestdatabasev2');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+ console.log("connected to mongo");
+});
+
+var bookSchema = new mongoose.Schema({
+ id: Number,
+ first_name: String
+ 
+});
+
+// “compile” the schema into a model
+var Book = mongoose.model('users',bookSchema);
+
+// create an express app
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
+// tell node to use json and HTTP header features in body-parser
+app.use(parser.json());
+app.use(parser.urlencoded({extended: true}));
 
-// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  // Initialize the app.
-  var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-});
-
-// CONTACTS API ROUTES BELOW.
-
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
-
-/*  "/contacts"
- *    GET: finds all contacts
- *    POST: creates a new contact
- */
-
-app.get("/contacts", function(req, res) {
-});
-
-app.post("/contacts", function(req, res) {
-});
-
-/*  "/contacts/:id"
- *    GET: find contact by id
- *    PUT: update contact by id
- *    DELETE: deletes contact by id
- */
-
-app.get("/contacts/:id", function(req, res) {
-});
-
-app.put("/contacts/:id", function(req, res) {
-});
-
-app.delete("/contacts/:id", function(req, res) {
-});
-
-app.post("/contacts", function(req, res) {
-  var newContact = req.body;
-  newContact.createDate = new Date();
-
-  if (!(req.body.firstName || req.body.lastName)) {
-    handleError(res, "Invalid user input", "Must provide a first or last name.", 400);
-  }
-
-  db.collection(CONTACTS_COLLECTION).insertOne(newContact, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to create new contact.");
-    } else {
-      res.status(201).json(doc.ops[0]);
-    }
-  });
+// handle GET requests for [domain]/api/books e.g. return all books
+app.route('/api/users')
+ .get(function (req,resp) {
+ // use mongoose to retrieve all books from Mongo
+ Book.find({}, function(err, data) {
+ if (err) {
+ resp.json({ message: 'Unable to connect to books' });
+ } else {
+ // return JSON retrieved by Mongo as response
+ resp.json(data);
+ }
+ });
+ });
+ 
+ // Use express to listen to port
+let port = 8080;
+app.listen(port, function () {
+ console.log("Server running at port= " + port);
 });
