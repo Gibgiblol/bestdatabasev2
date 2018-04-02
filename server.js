d365ef43ddd5d42a6bce23a25835254e25eac3ca
Charles <charlesclei21@gmail.com>
2018-04-02 17:41:27 -0600

    Monthly close fix #1

--- server.js
+++ server.js
@@ -206,6 +206,7 @@ app.route('/api/price/:symbol')
                 var priceArray = data;
                 var tempArr = [];
                 var average = 0;
+                var tempNum = 0;
                 for (let x = 0; x < data.length; x++) {
                     //Get Date string and split it into parts. Ex. 2017-01-01 = 2017,01,01
                     let thisMonth = data[x].date.split("-");
@@ -213,12 +214,13 @@ app.route('/api/price/:symbol')
                     //If tempArr[1] is null, which means new month, set counter to 1, and insert new value
                     if (tempArr[parseInt(thisMonth[1])] == null) {
                         var countDays = 1;
+                        tempNum = 0;
                         tempArr[parseInt(thisMonth[1])] = data[x].close;
                     }
 
                     //if tempArr isn't empty, take previous value and add together, and place back in.
                     else {
-                        var tempNum = tempArr[parseInt(thisMonth[1])] + data[x].close;
+                        tempNum = tempArr[parseInt(thisMonth[1])] + data[x].close;
                         tempArr[parseInt(thisMonth[1])] = tempNum;
                         countDays++;
 
@@ -227,7 +229,7 @@ app.route('/api/price/:symbol')
                             let nextMonth = data[x + 1].date.split("-");
                             if (parseInt(thisMonth[1]) != parseInt(nextMonth[1])) {
                                 var tempNum = tempArr[parseInt(thisMonth[1])] / countDays;
-                                tempArr[parseInt(thisMonth[1])] = tempNum;
+                                tempArr[parseInt(thisMonth[1])] = (tempNum).toFixed(2);
                             }
                         }
 
